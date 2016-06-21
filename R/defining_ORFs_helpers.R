9aeef9b29f3fc3ceda010afeadedeb10e43d76e4
Kornel Labun <kornel.labun@gmail.com>
2016-06-21 15:44:59 +0200

    resize ORF

--- R/defining_ORFs_helpers.R
+++ R/defining_ORFs_helpers.R
@@ -51,25 +51,25 @@ find_in_frame_ORFs <- function(fastaSeq, startCodon = c("ATG"), stopCodon = c("T
     }
     stop_cod <- paste(stopCodon, collapse = "|")
     start_cod <- paste(startCodon, collapse = "|")
-    
+
     codpos <- paste("(?:", start_cod, ")(?:[ATGCN]{3}(?<!", stop_cod, ")){", minimumLength, ",}(?:", stop_cod, ")", sep = "")
-    
+
     frame <- gregexpr(codpos, fastaSeq, perl = T)
-    frmat <- lapply(frame, function(x) if (x[1] != -1) 
+    frmat <- lapply(frame, function(x) if (x[1] != -1)
         cbind((as.integer(x)), (attr(x, "match.length"))) else matrix(NA, ncol = 2, nrow = 0))
     gr <- IRanges(start = do.call("rbind", frmat)[, 1], width = do.call("rbind", frmat)[, 2])
-    
+
     while (frame[[1]][1] != -1 && !longestORF) {
         starts <- c(sapply(frame, as.integer))
         fastaSeq <- subchar(fastaSeq, starts)
         frame <- gregexpr(codpos, fastaSeq, perl = T)
-        frmat <- lapply(frame, function(x) if (x[1] != -1) 
+        frmat <- lapply(frame, function(x) if (x[1] != -1)
             cbind((as.integer(x)), (attr(x, "match.length"))) else matrix(NA, ncol = 2, nrow = 0))
         frGR <- IRanges(start = do.call("rbind", frmat)[, 1], width = do.call("rbind", frmat)[, 2])
-        
+
         gr <- c(gr, frGR)
     }
-    
+
     gr <- gr[order(start(gr), end(gr))]
     gr
 }
@@ -89,23 +89,23 @@ find_in_frame_ORFs <- function(fastaSeq, startCodon = c("ATG"), stopCodon = c("T
 
 map_to_GRanges <- function(ORFdef, grangesObj) {
     returnList <- c()
-    
+
     if (length(ORFdef) == 0) {
         return(returnList)
     }
-    
+
     for (i in 1:length(ORFdef)) {
         ORFranges <- IRanges()
         startingPos <- start(ORFdef[i]) - 1
         endingPos <- end(ORFdef[i]) - 1
-        
+
         j = 1
         while (startingPos > width(grangesObj)[j]) {
             startingPos <- startingPos - width(grangesObj)[j]
             endingPos <- endingPos - width(grangesObj)[j]
             j = j + 1
         }
-        
+
         while (endingPos > width(grangesObj)[j]) {
             ORFranges <- c(ORFranges, IRanges(start = start(grangesObj)[j] + startingPos, end = end(grangesObj)[j]))
             startingPos <- 0
@@ -113,7 +113,7 @@ map_to_GRanges <- function(ORFdef, grangesObj) {
             j = j + 1
         }
         ORFranges <- c(ORFranges, IRanges(start = start(grangesObj)[j] + startingPos, end = start(grangesObj)[j] + endingPos))
-        
+
         chrom <- unique(as.character(seqnames(grangesObj)))
         strands <- unique(as.character(strand(grangesObj)))
         if (length(chrom) != 1) {
@@ -122,13 +122,34 @@ map_to_GRanges <- function(ORFdef, grangesObj) {
         if (length(strands) != 1) {
             stop("Different strands in GRanges object")
         }
-        
-        ORFranges <- GRanges(seqnames = Rle(rep(chrom, length(ORFranges))), ranges = ORFranges, strand = Rle(strand(rep(strands, 
+
+        ORFranges <- GRanges(seqnames = Rle(rep(chrom, length(ORFranges))), ranges = ORFranges, strand = Rle(strand(rep(strands,
             length(ORFranges)))))
         GenomeInfoDb::seqlevels(ORFranges) <- GenomeInfoDb::seqlevels(grangesObj)
         returnList <- c(returnList, ORFranges)
     }
-    
+
     returnList
 }
 
+#' Resizes down ORF to the desired length, removing inside. Preserves exons.
+#'
+#'  @export
+#'  @param grangesObj A GRanges object of ORF.
+#'  @param orf_goal_length numeric. Desired length of ORF.
+#'  @return GRanges object of resized ORF
+resize_ORF <- function(grangesObj, orf_goal_length){
+  length_diff <- (sum(width(grangesObj))/3 - orf_goal_length) * 3
+  is_even <- (length_diff %% 2) == 0
+  left_diff <- ifelse(is_even, length_diff/2, (length_diff - 1)/2)
+  right_diff <- ifelse(is_even, length_diff/2, (length_diff + 1)/2)
+
+  tiled <- tile(grangesObj, width = 1)
+  tiled <- flatGrl(tiled)
+  middle <- floor(length(tiled)/2)
+
+  tiled <- tiled[-c((middle - left_diff):(middle + right_diff - 1))]
+  tiled <- reduce(tiled)
+
+  return(tiled)
+}
