8cfcaacf5354184bf092252cc9798dd636223fac
Roleren <hauken_heyken@hotmail.com>
2017-12-12 15:57:13 +0100

    space fixes and small fixes

--- R/GRanges_helpers.R
+++ R/GRanges_helpers.R
@@ -1,26 +1,20 @@
-#' make GRangesList from GRanges, grouped by names
-#' @param gr a GRanges object
-GroupGRangesByNames <- function(gr){
-  if (class(gr) != "GRanges") stop("gr must be GRanges Object")
-  if (is.null(names(gr))) stop("gr object have no names")
-  l <- Rle(names(gr))
-  t <- unlist(lapply(1:nrun(l),function(x){ rep(x,runLength(l)[x])}))
-  grl <- split(gr,t)
-  names(grl) <- unique(names(gr))
-  return(grl)
-}
-
-#' make GRangesList from GRanges, grouped by other
+#' make GRangesList from GRanges, grouped by names or another column(other)
+#' ig. if GRanges should be grouped by gene, give gene column as other
 #' @param gr a GRanges object
 #' @param other a vector of names to group by
-GroupGRangesByOther <- function(gr, other){
+groupGRangesBy <- function(gr,other = NULL){
   if (class(gr) != "GRanges") stop("gr must be GRanges Object")
-  if (length(gr) != length(other))
-    stop(" in GroupGRangesByOther: lengths of gr and other does not match")
-  l <- Rle(other)
-  t <- unlist(lapply(1:nrun(l),function(x){ rep(x,runLength(l)[x])}))
-  grl <- split(gr,t)
-  names(grl) <- unique(other)
+  if(is.null(other)){ # if not using other
+    if (is.null(names(gr))) stop("gr object have no names")
+    l <- S4Vectors::Rle(names(gr))
+  } else { # else use other
+    if (length(gr) != length(other))
+      stop(" in GroupGRangesByOther: lengths of gr and other does not match")
+    l <- S4Vectors::Rle(other)
+  }
+  grouping <- unlist(lapply(1:nrun(l), function(x){ rep(x, runLength(l)[x])}))
+  grl <- split(gr, grouping)
+  names(grl) <- unique(names(gr))
   return(grl)
 }
 
@@ -31,7 +25,7 @@ widthPerGroup = function(grl, keep.names = T){
   if (class(grl) != "GRangesList") stop("grl must be GRangesList Object")
   if (keep.names){
     return(sum(width(grl)))
-  }else{
+  } else {
     return(as.integer(sum(width(grl))))
   }
 }
@@ -42,9 +36,9 @@ widthPerGroup = function(grl, keep.names = T){
 seqnamesPerGroup <- function(grl, keep.names = T){
   if (class(grl) != "GRangesList") stop("grl must be GRangesList Object")
   if (keep.names){
-    return(seqnames(phead(grl,1L)))
-  }else{
-    return(as.character(seqnames(phead(grl,1L))))
+    return(seqnames(phead(grl, 1L)))
+  } else {
+    return(as.character(seqnames(phead(grl, 1L))))
   }
 }
 
@@ -54,7 +48,6 @@ seqnamesPerGroup <- function(grl, keep.names = T){
 #'   ->T or min-> default(F) ?
 #' @importFrom data.table as.data.table
 gSort <- function(grl, decreasing = F){
-  if (class(grl) != "GRangesList") stop("grl must be GRangesList Object")
   DT <- as.data.table(grl)
   if (decreasing){
     asgrl <- makeGRangesListFromDataFrame(
@@ -77,14 +70,16 @@ gSort <- function(grl, decreasing = F){
 sortPerGroup <- function(grl, ignore.strand = F){
   if (class(grl) != "GRangesList") stop("grl must be GRangesList Object")
   if (!ignore.strand){
-    indexesPos <- which(strandPerGroup(grl,F) == "+")
-    indexesMin <- which(strandPerGroup(grl,F) == "-")
+    indexesPos <- which(strandPerGroup(grl, F) == "+")
+    indexesMin <- which(strandPerGroup(grl, F) == "-")
 
     grl[indexesPos] <- gSort(grl[indexesPos])
     grl[indexesMin] <- gSort(grl[indexesMin], decreasing = T)
 
     return(grl)
-  } else return(gSort(grl))
+  } else {
+    return(gSort(grl))
+  }
 }
 
 #' get list of strands per granges group
@@ -93,9 +88,9 @@ sortPerGroup <- function(grl, ignore.strand = F){
 strandPerGroup <- function(grl, keep.names = T){
   if (class(grl) != "GRangesList") stop("grl must be GRangesList Object")
   if (keep.names){
-    return(strand(phead(grl,1L)))
+    return(strand(phead(grl, 1L))) # S4Vectors::phead() wrapper
   }else{
-    return(as.character(strand(phead(grl,1L))))
+    return(as.character(strand(phead(grl, 1L))))
   }
 }
 
@@ -103,14 +98,14 @@ strandPerGroup <- function(grl, keep.names = T){
 #' @param grl a GRangesList
 firstExonPerGroup <- function(grl){
   if (class(grl) != "GRangesList") stop("grl must be GRangesList Object")
-  return(phead(grl,1L))
+  return(phead(grl, 1L)) # S4Vectors::phead() wrapper
 }
 
 #' get last exon per granges group
 #' @param grl a GRangesList
 lastExonPerGroup <- function(grl){
   if (class(grl) != "GRangesList") stop("grl must be GRangesList Object")
-  return(ptail(grl,1L))
+  return(ptail(grl,1L)) # S4Vectors::ptail() wrapper
 }
 
 #' get first start per granges group
@@ -119,9 +114,9 @@ lastExonPerGroup <- function(grl){
 firstStartPerGroup <- function(grl, keep.names = T){
   if (class(grl) != "GRangesList") stop("grl must be GRangesList Object")
   if (keep.names){
-    return( start(firstExonPerGroup(grl)))
+    return(start(firstExonPerGroup(grl)))
   }else{
-    return( as.integer(start(firstExonPerGroup(grl))))
+    return(as.integer(start(firstExonPerGroup(grl))))
   }
 }
 
@@ -131,9 +126,9 @@ firstStartPerGroup <- function(grl, keep.names = T){
 lastExonEndPerGroup = function(grl,keep.names = T){
   if (class(grl) != "GRangesList") stop("grl must be GRangesList Object")
   if (keep.names){
-    return(end(ptail(grl,1L)))
+    return(end(ptail(grl, 1L))) # S4Vectors::ptail() wrapper
   }else{
-    return( as.integer(end(ptail(grl,1L))))
+    return(as.integer(end(ptail(grl, 1L))))
   }
 }
 
@@ -144,15 +139,23 @@ numExonsPerGroup <- function(grl, keep.names = T){
   if (class(grl) != "GRangesList") stop("grl must be GRangesList Object")
 
   if (!is.null(names(unlist(grl, use.names = F)))){
-    exonsPerGroup <- Rle(names(unlist(grl, use.names = F)))
-    return(runLength(exonsPerGroup))
+    exonsPerGroup <- runLength(S4Vectors::Rle(names(unlist(grl,
+      use.names = F))))
   } else if (!is.null(names(unlist(grl)))){
-    exonsPerGroup <- Rle(names(unlist(grl)))
-    return(runLength(exonsPerGroup))
+    exonsPerGroup <- runLength(S4Vectors::Rle(names(unlist(grl,
+      use.names = T))))
   } else stop("no names to group exons")
+  if(keep.names){
+    if(is.null(names(grl))) stop("grl must have names if keep.names == T")
+    names(exonsPerGroup) = names(grl)
+  }
+  return(exonsPerGroup)
 }
 
 #' seqnames cleanup
+#' For many datasets, the fa file and the gtf file have different naming
+#' This functions tries to fix the naming to the GRanges standard
+#' chrX instead of X chr1 instead of 1 etc..
 #' @param grl a GRangesList
 fixSeqnames <- function(grl){
   if (class(grl) != "GRangesList") stop("grl must be GRangesList Object")
@@ -160,10 +163,10 @@ fixSeqnames <- function(grl){
   seqnamesTransformed <- as.character(seqnames(temp))
   indexes <- which(nchar(seqnamesTransformed) < 6)
   temp <- temp[indexes]
-  seqlevels(temp) <- sub(replacement = "chrY",pattern = "Y",seqlevels(temp))
-  seqlevels(temp) <- sub(replacement = "chrX",pattern = "X",seqlevels(temp))
+  seqlevels(temp) <- sub(replacement = "chrY", pattern = "Y", seqlevels(temp))
+  seqlevels(temp) <- sub(replacement = "chrX", pattern = "X", seqlevels(temp))
   seqlevels(temp) <- as.character(unique(seqnames(unlist(temp))))
-  return(GroupGRangesByNames(temp))
+  return(groupGRangesBy(temp))
 }
 
 #' make a meta column with exon ranks
@@ -185,7 +188,7 @@ makeExonRanks <- function(grl, byTranscript = F){
   if (!byTranscript){
     for (x in 2:length(t)) {
       if (t[x] == t[x - 1]) {
-        Inds[x] <- Inds[x-1] + 1
+        Inds[x] <- Inds[x - 1] + 1
       }
     }
   } else {
@@ -194,7 +197,9 @@ makeExonRanks <- function(grl, byTranscript = F){
         if (oldNames[t[x]] == oldNames[t[x] - 1]){
           Inds[x] <- Inds[x - 1] + 1
         }
-      } else Inds[x] = Inds[x - 1]
+      } else{
+        Inds[x] <- Inds[x - 1]
+      }
     }
   }
   return(Inds)
@@ -208,6 +213,6 @@ makeORFNames <- function(grl){
 
   asGR <- unlist(grl, use.names = F)
   if (is.null(names(asGR))) asGR <- unlist(grl, use.names = T)
-   asGR$names <- paste0(names(asGR), "_", ranks)
-  return(GroupGRangesByNames(asGR))
+    asGR$names <- paste0(names(asGR), "_", ranks)
+  return(groupGRangesBy(asGR))
 }
--- tests/testthat/test_ORFs_helpers.R
+++ tests/testthat/test_ORFs_helpers.R
@@ -2,8 +2,9 @@ library(ORFik)
 context("ORF helpers")
 
 transcriptRanges <- GRanges(seqnames = Rle(rep("1", 5)),
-                            ranges = IRanges(start = c(1, 10, 20, 30, 40), end = c(5, 15, 25, 35, 45)),
-                            strand = Rle(strand(rep("+", 5))))
+                            ranges = IRanges(start = c(1, 10, 20, 30, 40),
+                              end = c(5, 15, 25, 35, 45)),
+                                strand = Rle(strand(rep("+", 5))))
 ORFranges <- GRanges(seqnames = Rle(rep("1", 3)),
                      ranges = IRanges(start = c(1, 10, 20), end = c(5, 15, 25)),
                      strand = Rle(strand(rep("+", 3))))
@@ -40,17 +41,21 @@ test_that("define_trailer works as intended for plus strand", {
 
 
 transcriptRanges <- GRanges(seqnames = Rle(rep("1", 5)),
-                            ranges = IRanges(start = rev(c(1, 10, 20, 30, 40)), end = rev(c(5, 15, 25, 35, 45))),
-                            strand = Rle(strand(rep("-", 5))))
+                            ranges = IRanges(start = rev(c(1, 10, 20, 30, 40)),
+                              end = rev(c(5, 15, 25, 35, 45))),
+                                strand = Rle(strand(rep("-", 5))))
 ORFranges <- GRanges(seqnames = Rle(rep("1", 3)),
-                     ranges = IRanges(start = rev(c(1, 10, 20)), end = rev(c(5, 15, 25))),
-                     strand = Rle(strand(rep("-", 3))))
+                      ranges = IRanges(start = rev(c(1, 10, 20)),
+                        end = rev(c(5, 15, 25))),
+                          strand = Rle(strand(rep("-", 3))))
 ORFranges2 <- GRanges(seqnames = Rle(rep("1", 3)),
-                      ranges = IRanges(start = rev(c(10, 20, 30)), end = rev(c(15, 25, 35))),
-                      strand = Rle(strand(rep("-", 3))))
+                      ranges = IRanges(start = rev(c(10, 20, 30)),
+                        end = rev(c(15, 25, 35))),
+                          strand = Rle(strand(rep("-", 3))))
 ORFranges3 <- GRanges(seqnames = Rle(rep("1", 3)),
-                      ranges = IRanges(start = rev(c(20, 30, 40)), end = rev(c(25, 35, 45))),
-                      strand = Rle(strand(rep("-", 3))))
+                      ranges = IRanges(start = rev(c(20, 30, 40)),
+                        end = rev(c(25, 35, 45))),
+                          strand = Rle(strand(rep("-", 3))))
 
 test_that("define_trailer works as intended for minus strand", {
 
@@ -77,8 +82,9 @@ test_that("define_trailer works as intended for minus strand", {
 })
 
 transcriptRanges <- GRanges(seqnames = Rle(rep("1", 4)),
-                            ranges = IRanges(start = rev(c(10, 20, 30, 40)), end = rev(c(15, 25, 35, 45))),
-                            strand = Rle(strand(rep("-", 4))))
+                            ranges = IRanges(start = rev(c(10, 20, 30, 40)),
+                              end = rev(c(15, 25, 35, 45))),
+                                strand = Rle(strand(rep("-", 4))))
 
 
 
@@ -138,24 +144,25 @@ test_that("find_in_frame_ORFs works as intended for plus strand", {
 
 
 # Create data for get_all_ORFs_as_GRangesList test_that#1
-seqname <- c("tx1","tx2","tx3","tx4")
-seqs <- c("ATGGGTATTTATA","ATGGGTAATA","ATGGG", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+seqname <- c("tx1", "tx2", "tx3", "tx4")
+seqs <- c("ATGGGTATTTATA", "ATGGGTAATA",
+          "ATGGG", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
 grIn1 <- GRanges(seqnames = rep("1", 2),
                  ranges = IRanges(start = c(21, 10), end = c(23, 19)),
-                 strand = rep("-", 2), names = rep(seqname[1],2))
+                 strand = rep("-", 2), names = rep(seqname[1], 2))
 grIn2 <- GRanges(seqnames = rep("1", 1),
                  ranges = IRanges(start = c(1010), end = c(1019)),
-                 strand = rep("-", 1), names = rep(seqname[2],1))
+                 strand = rep("-", 1), names = rep(seqname[2], 1))
 
 grIn3 <- GRanges(seqnames = rep("1", 1),
                  ranges = IRanges(start = c(2000), end = c(2004)),
-                 strand = rep("-", 1), names = rep(seqname[3],1))
+                 strand = rep("-", 1), names = rep(seqname[3], 1))
 
 grIn4 <- GRanges(seqnames = rep("1", 2),
-                 ranges = IRanges(start = c(3030,3000), end = c(3036,3029)),
-                 strand = rep("-", 2), names = rep(seqname[4],2))
+                 ranges = IRanges(start = c(3030, 3000), end = c(3036, 3029)),
+                 strand = rep("-", 2), names = rep(seqname[4], 2))
 
-grl <- GRangesList(grIn1,grIn2,grIn3,grIn4)
+grl <- GRangesList(grIn1, grIn2, grIn3, grIn4)
 names(grl) <- seqname
 
 test_that("get_all_ORFs works as intended for minus strand", {
@@ -170,24 +177,24 @@ test_that("get_all_ORFs works as intended for minus strand", {
   expect_is(test_ranges, "GRangesList")
   expect_is(strand(test_ranges),"CompressedRleList")
   expect_is(seqnames(test_ranges),"CompressedRleList")
-  expect_equal(ORFik:::strandPerGroup(test_ranges,F)[1], "-")
-  expect_equal(as.integer(unlist(start(test_ranges))), c(10, 21, 1011, 1010,1012))
-  expect_equal(as.integer(unlist(end(test_ranges))), c(19, 22, 1019, 1018,1017))
+  expect_equal(strandPerGroup(test_ranges,F)[1], "-")
+  expect_equal(as.integer(unlist(start(test_ranges))), c(10, 21, 1011, 1010, 1012))
+  expect_equal(as.integer(unlist(end(test_ranges))), c(19, 22, 1019, 1018, 1017))
   expect_equal(as.integer(unlist(width(test_ranges))), c(10, 2, 9, 9, 6))
   expect_equal(sum(ORFik:::widthPerGroup(test_ranges) %% 3), 0)
 })
 
 
 # Create data for get_all_ORFs_as_GRangesList test_that#2
-namesTx <- c("tx1","tx2")
-seqs <- c("ATGATGTAATAA","ATGTAA")
+namesTx <- c("tx1", "tx2")
+seqs <- c("ATGATGTAATAA", "ATGTAA")
 grIn1 <- GRanges(seqnames = rep("1", 2),
                  ranges = IRanges(start = c(1, 3), end = c(1, 13)),
-                 strand = rep("+", 2), names = rep(namesTx[1],2))
+                 strand = rep("+", 2), names = rep(namesTx[1], 2))
 grIn2<- GRanges(seqnames = rep("1", 6),
-                 ranges = IRanges(start = c(1, 1000,2000,3000,4000,5000),
-                                  end = c(1, 1000,2000,3000,4000,5000)),
-                 strand = rep("+", 6), names = rep(namesTx[2],6))
+                 ranges = IRanges(start = c(1, 1000, 2000, 3000, 4000, 5000),
+                                  end = c(1, 1000, 2000, 3000, 4000, 5000)),
+                 strand = rep("+", 6), names = rep(namesTx[2], 6))
 grl <- GRangesList(grIn1, grIn2)
 names(grl) <- namesTx
 test_that("map_to_GRanges works as intended for strange exons positive strand", {
@@ -203,12 +210,16 @@ test_that("map_to_GRanges works as intended for strange exons positive strand", 
   expect_is(strand(test_ranges),"CompressedRleList")
   expect_is(seqnames(test_ranges),"CompressedRleList")
   expect_equal(ORFik:::strandPerGroup(test_ranges,F)[1], "+")
-  expect_equal(as.integer(unlist(start(test_ranges))), c(1, 3, 5,1,1000,2000,3000,4000,5000))
-  expect_equal(as.integer(unlist(end(test_ranges))), c(1, 10, 10,1,1000,2000,3000,4000,5000))
+  expect_equal(as.integer(unlist(start(test_ranges))), c(1, 3, 5,1, 1000, 2000,
+                                                         3000, 4000, 5000))
+  expect_equal(as.integer(unlist(end(test_ranges))), c(1, 10, 10,1, 1000, 2000,
+                                                       3000, 4000, 5000))
   expect_equal(sum(ORFik:::widthPerGroup(test_ranges) %% 3), 0)
-  expect_equal(unlist(grl)$names,c("tx1", "tx1", "tx2", "tx2", "tx2", "tx2", "tx2", "tx2"))
-  expect_equal(unlist(test_ranges)$names,c("tx1_1", "tx1_1", "tx1_2", "tx2_1", "tx2_1",
-                                   "tx2_1", "tx2_1", "tx2_1", "tx2_1"))
+  expect_equal(unlist(grl)$names,c("tx1", "tx1", "tx2", "tx2", "tx2", "tx2",
+                                   "tx2", "tx2"))
+  expect_equal(unlist(test_ranges)$names,c("tx1_1", "tx1_1", "tx1_2", "tx2_1",
+                                           "tx2_1", "tx2_1", "tx2_1", "tx2_1",
+                                           "tx2_1"))
 })
 
 # Create data for get_all_ORFs_as_GRangesList test_that#3
@@ -234,25 +245,28 @@ test_that("map_to_GRanges works as intended for strange exons negative strand", 
   expect_is(strand(test_ranges),"CompressedRleList")
   expect_is(seqnames(test_ranges),"CompressedRleList")
   expect_equal(ORFik:::strandPerGroup(test_ranges,F)[1], "-")
-  expect_equal(as.integer(unlist(start(test_ranges))), c(5, 5, 5000,4000,3000,2000,1000,1))
-  expect_equal(as.integer(unlist(end(test_ranges))), c(13, 10, 5000,4000,3000,2000,1000,1))
+  expect_equal(as.integer(unlist(start(test_ranges))), c(5, 5, 5000, 4000, 3000,
+                                                         2000, 1000, 1))
+  expect_equal(as.integer(unlist(end(test_ranges))), c(13, 10, 5000, 4000, 3000,
+                                                       2000, 1000, 1))
   expect_equal(sum(ORFik:::widthPerGroup(test_ranges) %% 3), 0)
-  expect_equal(unlist(grl)$names,c("tx1", "tx1", "tx2", "tx2", "tx2", "tx2", "tx2", "tx2"))
+  expect_equal(unlist(grl)$names,c("tx1", "tx1", "tx2", "tx2", "tx2",
+                                   "tx2", "tx2", "tx2"))
   expect_equal(unlist(test_ranges)$names,c("tx1_1","tx1_2", "tx2_1", "tx2_1",
                                    "tx2_1", "tx2_1", "tx2_1", "tx2_1"))
 })
 
-namesTx <- c("tx1","tx2", "tx3", "tx4")
-seqs <- c("ATGATGTAATAA","ATGTAA", "AAAATGAAATAAA","AAAATGAAATAA")
+namesTx <- c("tx1", "tx2", "tx3", "tx4")
+seqs <- c("ATGATGTAATAA", "ATGTAA", "AAAATGAAATAAA", "AAAATGAAATAA")
 
 
 grIn3 <- GRanges(seqnames = rep("1", 2),
-                 ranges = IRanges(start = c(2000,2008), end = c(2004,2015)),
-                 strand = rep("+", 2), names = rep(namesTx[3],2))
+                 ranges = IRanges(start = c(2000, 2008), end = c(2004, 2015)),
+                 strand = rep("+", 2), names = rep(namesTx[3], 2))
 grIn4 <- GRanges(seqnames = rep("1", 2),
-                 ranges = IRanges(start = c(3030,3000), end = c(3036,3004)),
-                 strand = rep("-", 2), names = rep(namesTx[4],2))
-grl <- GRangesList(grIn1, grIn2,grIn3, grIn4)
+                 ranges = IRanges(start = c(3030, 3000), end = c(3036, 3004)),
+                 strand = rep("-", 2), names = rep(namesTx[4], 2))
+grl <- GRangesList(grIn1, grIn2, grIn3, grIn4)
 names(grl) <- namesTx
 
 test_that("map_to_GRanges works as intended for strange exons both strands", {
