8bc02c3a2b3afbc036e6013ee8dbca1c54b21808
Håkon Tjeldnes <hauken_heyken@hotmail.com>
2019-02-08 14:11:04 +0100

    added new features, more tests and documentation (#70)
    * added new features, more tests and documentation
    
    * fixed space
    
    * made docs more clear and spaces

--- R/ORFs_helpers.R
+++ R/ORFs_helpers.R
@@ -318,6 +318,38 @@ stopCodons <- function(grl, is.sorted = FALSE) {
   return(grl)
 }
 
+#' Start region as GRangesList
+#'
+#' Get the start region of each ORF. If you want the start codon only,
+#' set upstream = 0 or just use \code{\link{startCodons}}.
+#' Standard is 2 upstream and 2 downstream, a width 5 window centered at
+#' start site. since p-shifting is not 100% accurate, this window is
+#' usually the reads from the start site.
+#'
+#' If tx is null, then upstream will be forced to 0 and downstream to
+#' a maximum of grl width. Since there is no reference for splicing.
+#' @param grl a \code{\link{GRangesList}} object
+#'  with usually either leaders, cds', 3' utrs or ORFs
+#' @param is.sorted logical (TRUE), is grl sorted.
+#' @param tx default NULL, a GRangesList of transcripts or (container region),
+#' names of tx must contain all grl names. The names of grl can also be the
+#' ORFik orf names. that is "txName_id"
+#' @inheritParams windowPerGroup
+#' @family features
+#' @return a GRanges, or GRangesList object if any group had > 1 exon.
+startRegion <- function(grl, tx = NULL, is.sorted = TRUE,
+                        upstream = 2L, downstream = 2L) {
+  if (!is.sorted) grl <- sortPerGroup(grl)
+  if (is.null(tx)) {
+    tx <- grl
+    names(tx) <- txNames(tx)
+  }
+  region <- windowPerGroup(startSites(grl, TRUE, TRUE, TRUE),
+                             tx, upstream, downstream)
+
+  return(region)
+}
+
 
 #' Get id's for each orf
 #'
