e6079e8433fd90cd8fc682e9f2b1162d145d6596
Roleren <hauken_heyken@hotmail.com>
2023-08-08 10:34:02 +0200

    cleaned up custom download for annotation

--- R/custom_download.R
+++ R/custom_download.R
@@ -10,34 +10,14 @@ custom_download <- function(url, ...) {
   withr::local_options(timeout = max(30000000, getOption("timeout")))
 
     if (RCurl::url.exists(url)) {
-        operating_sys <- Sys.info()[1]
-
-        if (operating_sys == "Darwin") {
-            downloader::download(
-                url = url, ...,
-                cacheOK = FALSE,
-                quiet = TRUE
-            )
-
-        }
-
-        if (operating_sys == "Linux") {
-            downloader::download(
-                url = url, ...,
-                cacheOK = FALSE,
-                quiet = TRUE
-            )
-        }
-
-        if (operating_sys == "Windows") {
-            downloader::download(url = url, ...,
-                                 method = "internal",
-                                 cacheOK = FALSE,
-                                 quiet = TRUE)
-        }
+      downloader::download(url = url, ...,
+                           cacheOK = FALSE,
+                           quiet = TRUE)
     } else {
         message(
-            "The FTP link: '",url,"' seems not to be available at the moment. This might be due to an instable internet connection, a firewall issue, or wrong organism name. Could you please try to re-run the function to see whether it works now?"
+            "The FTP link: '",url,"' seems not to be available at the moment.
+            This might be due to an instable internet connection, a firewall issue, or wrong organism name.
+            Could you please try to re-run the function to see whether it works now?"
         )
         return(FALSE)
     }
