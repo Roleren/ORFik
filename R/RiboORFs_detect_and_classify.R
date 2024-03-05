a0245c9bdbbed2d1bf6c2e23963bf150685a73de
Roleren <hauken_heyken@hotmail.com>
2024-03-05 20:33:54 +0100

    Prepared getters for ribo-seq ORF prediction

--- R/RiboORFs_detect_and_classify.R
+++ R/RiboORFs_detect_and_classify.R
@@ -34,6 +34,38 @@ categorize_ORFs <- function(orfs_unl, groupings = strtoi(names(orfs_unl)), cds, 
   return(ORF_type)
 }
 
+#' Categorize and filter ORFs
+#' @inheritParams detect_ribo_orfs
+#' @param orfs IRanges of ORF coordinates
+#' @param map_to_gr logical, default TRUE (genomic coordinates mapping)
+#' @return Categorized and filtered ORFs as IRanges or GRanges depending on map_to_gr,
+#' with metacolumn called
+#' @noRd
+categorize_and_filter_ORFs <- function(orfs, ORF_categories_to_keep,
+                                       cds, mrna,
+                                       map_to_gr = TRUE) {
+  stopifnot(length(ORF_categories_to_keep) > 0)
+  stopifnot(all(ORF_categories_to_keep %in% c("uORF", "uoORF", "annotated", "NTE",
+                                              "NTT", "internal", "doORF", "dORF", "a_error")))
+
+  orfs_unl <- unlist(orfs, use.names = TRUE)
+  groupings <- strtoi(names(orfs_unl))
+  names(orfs_unl) <- NULL
+  tx_ORF_unique <- names(mrna)[as.integer(names(orfs))]
+
+  ORF_type <- categorize_ORFs(orfs_unl, groupings, cds, mrna)
+  mcols(orfs@unlistData)$ORF_type <- ORF_type
+  filter_keep <- ORF_type %in% ORF_categories_to_keep
+  orfs<- split(orfs@unlistData[filter_keep], groupings[filter_keep])
+  ORF_type_keep <- mcols(orfs@unlistData)$ORF_type
+
+  if (map_to_gr) {
+    orfs <- pmapFromTranscriptF(orfs, mrna, removeEmpty = TRUE)
+  }
+  mcols(orfs)$category <- ORF_type_keep
+  return(orfs)
+}
+
 coveragePerORFStatistics <- function(grl, RFP) {
   cov <- coveragePerTiling(grl, RFP, is.sorted = TRUE, as.data.table = TRUE, withFrames = TRUE)
   cov_stats <- cov[, .(sum = sum(count), median= median(count), mean = mean(count)), by = genes]
@@ -89,7 +121,7 @@ coveragePerORFStatistics <- function(grl, RFP) {
 #'  \item{a_error: }{Any ORF detect not in the above categories}
 #' }
 #' @param prefix_result the prefix name of output files to out_folder. Default:
-#' \code{paste(c(ORF_categories_to_keep, organism(df)), collapse = "_")}
+#' \code{paste(c(ORF_categories_to_keep, gsub(" ", "_", organism(df))), collapse = "_")}
 #' @param mrna = \code{loadRegion(df, "mrna")}
 #' @param cds = \code{loadRegion(df, "cds")}
 #' @param libraries the ribo-seq libraries loaded into R as list, default:
@@ -115,17 +147,34 @@ coveragePerORFStatistics <- function(grl, RFP) {
 #' # EXAMPLE:
 #' df <- ORFik.template.experiment()
 #' df <- df[df$libtype == "RFP",][c(1,2),]
-#' result_folder <- file.path(tempdir(), paste0("Ribo_orfs_", organism(df)))
+#' result_folder <- riboORFsFolder(df, tempdir())
 #' results <- detect_ribo_orfs(df, result_folder, c("uORF", "uoORF", "annotated", "NTE"))
 #'
 #' # Load results of annotated ORFs
-#' res_path <- "uORF_uoORF_annotated_NTE_Homo sapiens_ORFik_RFP_r1_prediction_table.rds"
-#' res_path <- file.path(result_folder, res_path)
-#' annotated <- readRDS(annotated_path)
-#' annotated # See all statistics
-#' sum(annotated$predicted) # How many were predicted as Ribo-seq ORFs
+#' table <- riboORFs(df[1,], type = "table", result_folder)
+#' table # See all statistics
+#' sum(table$predicted) # How many were predicted as Ribo-seq ORFs
+#' # Load 2 results
+#' table <- riboORFs(df[1:2,], type = "table", result_folder)
+#' table # See all statistics
+#' sum(table$predicted) # How many were predicted as Ribo-seq ORFs
+#'
+#' # Load GRangesList
+#' candidates_gr <- riboORFs(df[1,], type = "ranges_candidates", result_folder)
+#' prediction <- riboORFs(df[1,], type = "predictions", result_folder)
+#'
+#' predicted_gr <- riboORFs(df[1:2,], type = "ranges_predictions", result_folder)
+#' identical(predicted_gr[[1]], candidates_gr[[1]][prediction[[1]]])
+#' ## Inspect predictions in RiboCrypt
+#' # library(RiboCrypt)
+#' # Inspect Predicted
+#' view <- predicted_gr[[1]][1]
+#' #multiOmicsPlot_ORFikExp(view, df, view, leader_extension = 100, trailer_extension = 100)
+#' # Inspect not predicted
+#' view <- candidates_gr[[1]][!prediction[[1]]][1]
+#' #multiOmicsPlot_ORFikExp(view, df, view, leader_extension = 100, trailer_extension = 100)
 detect_ribo_orfs <- function(df, out_folder, ORF_categories_to_keep,
-                             prefix_result = paste(c(ORF_categories_to_keep, organism(df)), collapse = "_"),
+                             prefix_result = paste(c(ORF_categories_to_keep, gsub(" ", "_", organism(df))), collapse = "_"),
                              mrna = loadRegion(df, "mrna"), cds = loadRegion(df, "cds"),
                              libraries = outputLibs(df, type = "pshifted", output = "envirlist"),
                              orf_candidate_ranges = findORFs(seqs = txSeqsFromFa(mrna, df, TRUE), longestORF = longestORF,
@@ -138,40 +187,28 @@ detect_ribo_orfs <- function(df, out_folder, ORF_categories_to_keep,
   start_timer <- Sys.time()
   message("Finding all candidate ORFs")
   dir.create(out_folder, recursive = TRUE, showWarnings = FALSE)
-  stopifnot(length(ORF_categories_to_keep) > 0)
-  stopifnot(all(ORF_categories_to_keep %in% c("uORF", "uoORF", "annotated", "NTE",
-                                              "NTT", "internal", "doORF", "dORF", "a_error")))
 
-  # New group you can start here
-  orfs <- orf_candidate_ranges
-  orfs_unl <- unlist(orfs, use.names = TRUE)
-  groupings <- strtoi(names(orfs_unl))
-  names(orfs_unl) <- NULL
-  tx_ORF_unique <- names(mrna)[as.integer(names(orfs))]
 
-  ORF_type <- categorize_ORFs(orfs_unl, groupings, cds, mrna)
-  mcols(orfs@unlistData)$ORF_type <- ORF_type
-  filter_keep <- ORF_type %in% ORF_categories_to_keep
-  orfs<- split(orfs@unlistData[filter_keep], groupings[filter_keep])
-  ORF_type_keep <- mcols(orfs@unlistData)$ORF_type
-
-  orfs_gr <- pmapFromTranscriptF(orfs, mrna, removeEmpty = TRUE)
-
-  # orfs_chr <- seqnamesPerGroup(orfs_gr, FALSE)
+  # Filter categories and map to GRanges
+  orfs_gr <- categorize_and_filter_ORFs(orf_candidate_ranges,
+                                        ORF_categories_to_keep, cds, mrna)
   orf_start_gr <- startSites(orfs_gr, TRUE,TRUE,TRUE)
   orf_stop_gr <- stopSites(orfs_gr, TRUE,TRUE,TRUE)
   # orf_start_overlaps_other <- countOverlaps(orf_start_gr, orfs_gr) - 1
 
   message("Start Ribo-seq coverage analysis")
-  libnames <- bamVarName(df)
+  libnames <- name_decider(df, naming = "full")
+  symbols <- suppressMessages(symbols(df))
+  ORF_type_keep <- mcols(orfs_gr)$category
+  mcols(orfs_gr) <- NULL
   out_file_prefixes <- file.path(out_folder, paste0(prefix_result,
-                                                   "_", name(df),
-                                                   "_", libnames))
+                                                    "_", name(df),
+                                                    "_", libnames))
   for (i in seq_along(libraries)) {
     message("- ", libnames[i])
     detect_ribo_orfs_single_cov(orfs_gr, libraries[[i]], out_file_prefixes[i],
                                 mrna, ORF_type_keep, orf_start_gr, orf_stop_gr,
-                                export_metrics_table,
+                                export_metrics_table, symbols,
                                 minimum_reads_ORF, minimum_reads_start)
 
   }
@@ -181,11 +218,13 @@ detect_ribo_orfs <- function(df, out_folder, ORF_categories_to_keep,
 }
 
 detect_ribo_orfs_single_cov <- function(orfs_gr, RFP, out_file_prefix, mrna,
-                                        ORF_type_keep,
+                                        ORF_type_keep = mcols(orfs_gr)$category,
                                         orf_start_gr = startSites(orfs_gr, TRUE,TRUE,TRUE),
                                         orf_stop_gr = stopSites(orfs_gr, TRUE,TRUE,TRUE),
-                                        export_metrics_table = TRUE,
+                                        export_metrics_table = TRUE, symbols,
                                         minimum_reads_ORF = 10, minimum_reads_start = 3) {
+  stopifnot(!is.null(ORF_type_keep))
+  if (ncol(mcols(orfs_gr)) > 0) mcols(orfs_gr) <- NULL
   # Filter candidates by counts
   message("-- Filtering")
   reads_10 <- countOverlapsW(orfs_gr, RFP, weight = "score") > minimum_reads_ORF
@@ -221,12 +260,82 @@ detect_ribo_orfs_single_cov <- function(orfs_gr, RFP, out_file_prefix, mrna,
 
   if(export_metrics_table) {
     start_codons <- txSeqsFromFa(startCodons(orfs_cand, TRUE), df, TRUE, keep.names = FALSE)
-    res <- cbind(gene = txNamesToGeneNames(names(orfs_cand), df), tx = names(orfs_cand),
-                 type = ORF_type_keep[candidates], predicted, length = widthPerGroup(orfs_cand, FALSE),
+
+    if (is(symbols, "data.table") && nrow(symbols) > 0 && "ensembl_tx_name" %in% colnames(symbols)) {
+      naming <- data.table::merge.data.table(data.table(ensembl_tx_name = names(orfs_cand)),
+                                             symbols, by = "ensembl_tx_name", all.x = TRUE, sort = FALSE)
+    } else naming <- data.table(gene = txNamesToGeneNames(names(orfs_cand), df), tx = names(orfs_cand))
+    res <- cbind(naming, type = ORF_type_keep[candidates], predicted, length = widthPerGroup(orfs_cand, FALSE),
                  start_codons, orfs_cov_stats, up = upstream_cov_stats, down = downstream_cov_stats)
     res[, `:=` (down.genes = NULL, up.genes = NULL)]
     setnames(res, "genes", "id")
     saveRDS(res, paste0(out_file_prefix, "_prediction_table.rds"))
   }
   return(invisible(NULL))
+}
+
+#' Define folder for prediction output
+#'
+#' @param df ORFik experiment
+#' @param parrent_dir Parrent directory of computed study results, default:
+#' resFolder(df)
+#' @return a file path (full path)
+#' @export
+#' @examples
+#' df <- ORFik.template.experiment()
+#' df <- df[df$libtype == "RFP",][c(1,2),]
+#' riboORFsFolder(df)
+#' riboORFsFolder(df, tempdir())
+riboORFsFolder <- function(df, parrent_dir = resFolder(df)) {
+  dir <- paste0("Ribo_orfs_", gsub(" ", "_", organism(df)))
+  return(file.path(parrent_dir, dir))
+}
+
+#' Load Predicted translons
+#'
+#' @param df ORFik experiment
+#' @param type default "table", alternatives: c("table", "ranges_candidates",
+#'  "ranges_predictions", "predictions")
+#' @param folder base folder to check for computed results, default:
+#'  riboORFsFolder(df)
+#' @return a data.table, GRangesList or list of logical vector depending on input
+#' @export
+#' @examples
+#' df <- ORFik.template.experiment()
+#' df <- df[df$libtype == "RFP",][c(1,2),]
+#' # riboORFs(df) # Works when you have run prediction
+riboORFs <- function(df, type = "table", folder = riboORFsFolder(df)) {
+  stopifnot(type %in% c("table", "ranges_candidates", "ranges_predictions", "predictions"))
+  stopifnot(is(df, "experiment"))
+  stopifnot(dir.exists(folder))
+  search_terms <- paste0(c("_prediction_table", "_candidates", "_candidates", "_prediction"), "\\.rds$")
+  names(search_terms) <- c("table", "ranges_candidates", "ranges_predictions", "predictions")
+  search_term <- search_terms[type]
+
+  file_paths <- list.files(folder, paste(search_term, collapse = "|"),
+                           full.names = TRUE)
+  libnames <- name_decider(df, naming = "full")
+  list <- lapply(seq_along(libnames), function(id) {
+    x <- libnames[id]
+    file <- grep(pattern = x, x = file_paths, value = TRUE)
+    if (length(file) > 1) {
+      print(file)
+      stop("multiple files found for this library, see above for which")
+    } else if (length(file) == 0) {
+      print(file_paths)
+      stop("Could not find file!")
+    }
+
+    rds <- readRDS(file)
+    if (type == "table") {
+      rds <- cbind(rds, library = x)
+      rds[]
+    } else if (type == "ranges_predictions") {
+      predictions <- riboORFs(df[id, ], "predictions", folder)
+      rds <- rds[predictions[[1]]]
+    }
+    return(rds)
+  })
+  if (type == "table") list <- rbindlist(list)
+  return(list)
 }
