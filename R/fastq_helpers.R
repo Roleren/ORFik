ba005cda1930270218f90d99cdd442207fc369a2
Roleren <hauken_heyken@hotmail.com>
2025-10-29 16:06:41 +0100

    Added working example to collapse function

--- R/fastq_helpers.R
+++ R/fastq_helpers.R
@@ -68,24 +68,28 @@ mergeFastq <- function(in_files, out_files, BPPARAM = bpparam()) {
 #' @export
 #' @examples
 #'
-#' fastq.folder <- tempdir() # <- Your fastq files
-#' infiles <- dir(fastq.folder, "*.fastq", full.names = TRUE)
-#' # collapse.fastq(infiles)
-#'
+#' seqs <- rep(c("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA", "GGGGGGGGGGGGGGGGGGGG"), times = c(2,3))
+#' fastq.folder <- tempfile()
+#' dir.create(fastq.folder)
+#' fastq.file <- file.path(fastq.folder, "test.fasta")
+#' writeXStringSet(DNAStringSet(seqs), filepath = fastq.file)
+#' infiles <- dir(fastq.folder, "fasta|fastq", full.names = TRUE)
+#' collapse.fastq(infiles)
+#' readDNAStringSet(file.path(fastq.folder, "collapsed", "collapsed_test.fasta"))
+#' # You see names says x3 of read 1 (GGGG...) and x2 of read 2 (AAAA....)
 collapse.fastq <- function(files, outdir = file.path(dirname(files[1]), "collapsed"),
                            header.out.format = "ribotoolkit", compress = FALSE,
                            prefix = "collapsed_") {
   if (!dir.exists(outdir)) {
     dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
   }
+  format <- get_fast_format(files)
 
-  format <- rep("fasta", length(files))
-  format[grep("\\.fastq|\\.fq", files)] <- "fastq"
 
   for (f in seq_along(files)) {
     file <- files[f]
     message("File ", f, "/", length(files), ":  ", file)
-    fasta_name <- gsub(pattern = "\\.fastq$", replacement = ".fasta",
+    fasta_name <- gsub(pattern = fastq_regex_pattern(), replacement = ".fasta",
                        basename(file))
     new_file_name <- paste0(prefix, fasta_name,
                             ifelse(compress, ".gz", ""))
@@ -119,4 +123,18 @@ collapse_header_set <- function(N, indices = seq_along(N),
     headers <- sprintf("seq%d_x%d", indices, N)
   } else stop("format must be 'fastx' or 'ribotoolkit'")
   return(headers)
+}
+
+get_fast_format <- function(files, allowed_compressions = "gz") {
+  format <- rep("fasta", length(files))
+  format[file_is_fastq(files, allowed_compressions)] <- "fastq"
+  return(format)
+}
+
+file_is_fastq <- function(files, allowed_compressions = "gz") {
+  grep(fastq_regex_pattern(allowed_compressions), files)
+}
+
+fastq_regex_pattern <- function(allowed_compressions = "gz") {
+  paste0("\\.(fastq|fq)(", paste("\\.", allowed_compressions, sep = "", collapse = "|"), ")?$")
 }
